e4d26c4107d2af08cd473e270c227395e2e29ef7
AmraniCh <elamrani.sv.laza@gmail.com>
2021-06-13 01:04:12 +0100

    font family input

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('#changeBtn').addEventListener('click', function() {
-        chrome.tabs.insertCSS({
-            code: `.blob-code-inner {
-                font-family: Consolas, sans-serif !important;
-            }`
-        });
-    }, false);
-}, false);
+addEvent(document, 'DOMContentLoaded', function () {
+    addEvent('#changeBtn', 'click', function () {
+        var fonts = document.querySelementctor('#font_family').value;
+        if (fonts) {
+            changeFont(fonts);
+        }
+    });
+});
+
+function changeFont(fonts) {
+    chrome.tabs.insertCSS({
+        code: `.blob-code-inner {
+            font-family: ${fonts} !important;
+        }`,
+    });
+}
+
+function addEvent(element, event, handler) {
+    if (typeof element === 'string' && /^\.|^#/.test(element)) {
+        element = document.querySelementctor(element);
+    }
+
+    element.addEventListener(event, handler.bind(this), false);
+}
