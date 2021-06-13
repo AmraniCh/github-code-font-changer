a90214ab63dfc9715f96dd98d7a747f50da07e0b
AmraniCh <elamrani.sv.laza@gmail.com>
2021-06-13 23:01:13 +0100

    Added some docs

--- src/background.js
+++ src/background.js
@@ -1,4 +1,6 @@
+// add a listener to onUpdated event
 chrome.tabs.onUpdated.addListener(function (tabId, info) {
+    // if the tab is completely loaded
     if (info.status === 'complete') {
         chrome.storage.sync.get('gt_code_fonts', function (data) {
             const fonts = data.gt_code_fonts;
@@ -7,6 +9,11 @@ chrome.tabs.onUpdated.addListener(function (tabId, info) {
     }
 });
 
+/**
+ * Changes font family for github code
+ * @param {String} fonts
+ * @returns true
+ */
 function changeFont(fonts) {
     chrome.tabs.insertCSS({
         code: `.blob-code-inner {
@@ -17,6 +24,12 @@ function changeFont(fonts) {
     return true;
 }
 
+/**
+ * Just a shortcut for the native target.addEventListener
+ * @param {DOMElement} ele
+ * @param {String} event
+ * @param {function} handler
+ */
 function addEvent(ele, event, handler) {
     ele.addEventListener(event, handler.bind(this), false);
 }
--- src/popup.js
+++ src/popup.js
@@ -1,20 +1,28 @@
-var background = chrome.extension.getBackgroundPage(),
+const background = chrome.extension.getBackgroundPage(),
+    /**
+     * Get variables from background
+     */
     addEvent = background.addEvent,
-    changeFont = background.changeFont;
+    changeFont = background.changeFont,
+    /**
+     * Popup DOM elements
+     */
+    applyBtn = document.querySelector('#change_btn'),
+    fontFamilyInput = document.querySelector('#font_family');
 
 addEvent(document, 'DOMContentLoaded', function () {
-    const applyBtn = document.querySelector('#change_btn'),
-        fontFamilyInput = document.querySelector('#font_family');
-
+    // handle apply button click event
     addEvent(applyBtn, 'click', function () {
         var fonts = fontFamilyInput.value;
         if (fonts && changeFont(fonts)) {
             applyBtn.textContent = 'Done!';
+            // store the fonts in browser storage
             chrome.storage.sync.set({ gt_code_fonts: fonts });
         }
     });
 });
 
+// returns to original apply button text if typing in the font family input
 addEvent(fontFamilyInput, 'input', function () {
     applyBtn.textContent = 'apply';
 });
