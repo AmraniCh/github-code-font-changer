ea33625ec33bd8ffae4a0f11d6e37ddc87efbc77
El Amrani Shakir <elamrani.sv.laza@gmail.com>
2021-06-14 10:34:53 +0100

    Populate options of select fonts dropdown

--- src/popup.js
+++ src/popup.js
@@ -8,10 +8,31 @@ const background = chrome.extension.getBackgroundPage(),
      * Popup DOM elements
      */
     applyBtn = document.querySelector('#change_btn'),
-    fontFamilyInput = document.querySelector('#font_family');
+    fontFamilyInput = document.querySelector('#font_family'),
+    selectFontDropdown = document.querySelector('#fonts_select'),
+    /**
+     * Select Fonts
+     */
+    fonts = {
+        'Fira Code': "url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap')",
+    };
 
 // popup document content loaded
 addEvent(document, 'DOMContentLoaded', function () {
+    // populate options of select fonts dropdown
+    for (var fontName in fonts) {
+        if (!Object.prototype.hasOwnProperty.call(fonts, fontName)) {
+            continue;
+        }
+
+        var option = document.createElement('option');
+
+        option.value = fontName;
+        option.textContent = fontName;
+
+        selectFontDropdown.appendChild(option);
+    }
+
     // fill the font family input with fonts from storage
     chrome.storage.sync.get('gt_code_fonts', function (data) {
         const fonts = data.gt_code_fonts;
