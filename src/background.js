04fbbd045a2a3b6feae2f64ab9abb71bed9e15bf
GamerGuy-52 <82700327+GamerGuy-52@users.noreply.github.com>
2021-09-10 11:21:46 +0400

    Added Fonts
    Added:
    - JetBrains Mono
    - Roboto Mono

--- src/background.js
+++ src/background.js
@@ -4,6 +4,8 @@ var /**
     fonts = {
         'Fira Code': 'https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap',
         'Source Code Pro': 'https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@200;300;400;500;600;700;900&display=swap',
+        'Roboto Mono': 'https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap',
+        'JetBrains Mono': 'https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap'
     },
     selectors = {
         code: '.blob-code-inner',
