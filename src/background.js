62cb7082e2ec833dafae50dec4777ca047c24395
Japroz Saini <sainijaproz@gmail.com>
2021-11-15 10:53:54 +0530

    Add menlo font
    This is the default monospace font on macOS and is a really nice monospace font overall.

--- src/background.js
+++ src/background.js
@@ -15,6 +15,7 @@ var /**
         'Space Mono': 'https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap',
         Inconsolata: 'https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&display=swap',
         'Anonymous Pro': 'https://fonts.googleapis.com/css2?family=Anonymous+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap',
+        'Menlo': 'https://fonts.cdnfonts.com/css/menlo',
         'IBM Plex Mono':
             'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap',
     },
